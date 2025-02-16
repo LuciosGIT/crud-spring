cf60c41f8aa1179d420c97f532f5b3b84dae3b79
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-02-16 16:09:41 -0300

    refactor : change id column name to align with frontend dataset

--- src/main/java/com/joselucio/crud_spring/model/Course.java
+++ src/main/java/com/joselucio/crud_spring/model/Course.java
@@ -1,5 +1,6 @@
 package com.joselucio.crud_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
@@ -11,6 +12,7 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
