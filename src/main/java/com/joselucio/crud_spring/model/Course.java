1c2e03439e67d2d1e07e39bf24f61099188bfa69
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-02-22 21:49:22 -0300

    feat : add soft course deletion

--- src/main/java/com/joselucio/crud_spring/model/Course.java
+++ src/main/java/com/joselucio/crud_spring/model/Course.java
@@ -8,10 +8,14 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@SQLRestriction("status <> 'Inativo'")
 public class Course {
 
     @Id
@@ -30,5 +34,11 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 
 }
