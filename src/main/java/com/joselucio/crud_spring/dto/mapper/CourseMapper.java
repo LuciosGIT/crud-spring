90e83370cc9ef3fb1ac82244142dad8afc0132f7
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-02-24 22:37:45 -0300

    refactor : implements categoryValue converter

--- src/main/java/com/joselucio/crud_spring/dto/mapper/CourseMapper.java
+++ src/main/java/com/joselucio/crud_spring/dto/mapper/CourseMapper.java
@@ -9,16 +9,16 @@ import org.springframework.stereotype.Component;
 public class CourseMapper {
 
     public CourseDTO toDTO(Course course) {
-        if(course == null) {
+        if (course == null) {
             return null;
         }
-        return new CourseDTO(course.getId(), course.getName(), "Front-end");
+        return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue());
 
     }
 
     public Course toEntity(CourseDTO courseDTO) {
 
-        if(courseDTO == null) {
+        if (courseDTO == null) {
             return null;
         }
 
@@ -26,9 +26,20 @@ public class CourseMapper {
         if (courseDTO.id() != null) {
             course.setId(courseDTO.id());
         }
-            course.setName(courseDTO.name());
-            course.setCategory(Category.FRONT_END);
+        course.setName(courseDTO.name());
+        course.setCategory(convertCategoryValue(courseDTO.category()));
         return course;
     }
 
+    public Category convertCategoryValue(String value) {
+        if (value == null) {
+            return null;
+        }
+        return switch (value) {
+            case "Front-end" -> Category.FRONT_END;
+            case "Back-end" -> Category.BACK_END;
+            default -> throw new IllegalArgumentException("Categoria inválida: " + value);
+
+        };
+    }
 }
