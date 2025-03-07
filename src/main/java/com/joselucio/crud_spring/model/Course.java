2e1f015468b3f25baa93a419e5ea0bc5906d25dc
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-07 17:05:36 -0300

    refactor : implements getters & setters manually

--- src/main/java/com/joselucio/crud_spring/model/Course.java
+++ src/main/java/com/joselucio/crud_spring/model/Course.java
@@ -6,9 +6,10 @@ import com.joselucio.crud_spring.enums.Status;
 import com.joselucio.crud_spring.enums.converters.CategoryConverter;
 import com.joselucio.crud_spring.enums.converters.StatusConverter;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
@@ -16,7 +17,6 @@ import org.hibernate.validator.constraints.Length;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
 @SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
 @SQLRestriction("status <> 'Inativo'")
@@ -43,7 +43,71 @@ public class Course {
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ACTIVE;
 
+    @NotNull
+    @NotEmpty
+    @Valid
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
     private List<Lesson> lessons = new ArrayList<>();
 
+    public Course(Category category, Long id, List<Lesson> lessons, String name, Status status) {
+        this.category = category;
+        this.id = id;
+        this.lessons = lessons;
+        this.name = name;
+        this.status = status;
+    }
+
+    public Course() {
+    }
+
+    public @NotNull Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(@NotNull Category category) {
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    public @NotBlank @NotNull @Length(min = 5, max = 100) String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank @NotNull @Length(min = 5, max = 100) String name) {
+        this.name = name;
+    }
+
+    public @NotNull Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(@NotNull Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "category=" + category +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", lessons=" + lessons +
+                '}';
+    }
 }
--- src/main/java/com/joselucio/crud_spring/model/Lesson.java
+++ src/main/java/com/joselucio/crud_spring/model/Lesson.java
@@ -2,9 +2,10 @@ package com.joselucio.crud_spring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
-@Data
 @Entity
 public class Lesson {
 
@@ -12,17 +13,73 @@ public class Lesson {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
     @Column(length = 100, nullable = false)
     private String name;
 
+    @NotBlank
+    @NotNull
+    @Length(min = 10, max = 11)
     @Column(length =11, nullable = false)
     private String youtubeUrl;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Course course;
 
+    public Lesson(Course course, Long id, String name, String youtubeUrl) {
+        this.course = course;
+        this.id = id;
+        this.name = name;
+        this.youtubeUrl = youtubeUrl;
+    }
 
+    public Lesson() {
+    }
 
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYoutubeUrl() {
+        return youtubeUrl;
+    }
+
+    public void setYoutubeUrl(String youtubeUrl) {
+        this.youtubeUrl = youtubeUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" +
+                "course=" + course +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", youtubeUrl='" + youtubeUrl + '\'' +
+                '}';
+    }
 }
