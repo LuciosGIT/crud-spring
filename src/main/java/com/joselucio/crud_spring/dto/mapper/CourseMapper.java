a82d47bdff6be969a05a3b6457375f6d308d10f7
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-06 23:09:01 -0300

    refactor : updates toEntity method to support lessons update and creation

--- src/main/java/com/joselucio/crud_spring/dto/mapper/CourseMapper.java
+++ src/main/java/com/joselucio/crud_spring/dto/mapper/CourseMapper.java
@@ -4,6 +4,7 @@ import com.joselucio.crud_spring.dto.CourseDTO;
 import com.joselucio.crud_spring.dto.LessonDTO;
 import com.joselucio.crud_spring.enums.Category;
 import com.joselucio.crud_spring.model.Course;
+import com.joselucio.crud_spring.model.Lesson;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -39,6 +40,18 @@ public class CourseMapper {
         }
         course.setName(courseDTO.name());
         course.setCategory(convertCategoryValue(courseDTO.category()));
+
+        List<Lesson> lessons = courseDTO.lessons().stream().map((lessonDTO) -> {
+            var lesson = new Lesson();
+            lesson.setId(lessonDTO.id());
+            lesson.setName(lessonDTO.name());
+            lesson.setYoutubeUrl(lessonDTO.youtubeUrl());
+            lesson.setCourse(course);
+            return lesson;
+        }).collect(Collectors.toList());
+
+        course.setLessons(lessons);
+
         return course;
     }
 
